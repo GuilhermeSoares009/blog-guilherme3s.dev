86e9330fe141edcf8fbbaa6d600dc150345b0025
Guilherme Soares Sousa Silva <guilherme.sousa009@hotmail.com>
2024-11-05 23:49:08 -0300

    Atualização

--- source/build/app.js
+++ source/build/app.js
@@ -1,9 +1,1 @@
-/******/ (() => { // webpackBootstrap
-/*!*********************************!*\
-  !*** ./source/assets/js/app.js ***!
-  \*********************************/
-// source/assets/js/app.js
-console.log('App carregado!');
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQTtBQUNBQSxPQUFPLENBQUNDLEdBQUcsQ0FBQyxnQkFBZ0IsQ0FBQyxDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYmxvZy1ndWlsaGVybWUzcy5kZXYvLi9zb3VyY2UvYXNzZXRzL2pzL2FwcC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBzb3VyY2UvYXNzZXRzL2pzL2FwcC5qc1xyXG5jb25zb2xlLmxvZygnQXBwIGNhcnJlZ2FkbyEnKTsiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
+console.log("App carregado!");
